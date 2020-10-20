0fd1f2ef08cd9e596bc8416e46e082aa2c13f8a2
yuqing.ji <yuqing.ji@outlook.com>
2020-10-21 06:48:00 +0800

    timestamp on pdf

--- subscriptions/hackernews.js
+++ subscriptions/hackernews.js
@@ -1,5 +1,13 @@
 const puppeteer = require('puppeteer');
 
+function getDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = `${date.getMonth() + 1}`.padStart(2, '0');
+  const day =`${date.getDate()}`.padStart(2, '0');
+  return `${year}-${month}-${day}`
+}
+
 (async () => {
   const browser = await puppeteer.launch({
     args:[
@@ -10,6 +18,6 @@ const puppeteer = require('puppeteer');
   });
   const page = await browser.newPage();
   await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
-  await page.pdf({path: '/pdf/hackernews.pdf', format: 'A4'});
+  await page.pdf({path: '/pdf/hackernews-' + getDate() + '.pdf', format: 'A4'});
   await browser.close();
 })();
