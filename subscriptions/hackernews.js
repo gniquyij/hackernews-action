0940a628cb3d89ec1d5db137441f2b54817b1737
yuqing.ji <yuqing.ji@outlook.com>
2020-10-21 07:31:27 +0800

    disable date for pdf name

--- subscriptions/hackernews.js
+++ subscriptions/hackernews.js
@@ -1,12 +1,12 @@
 const puppeteer = require('puppeteer');
 
-function getDate() {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = `${date.getMonth() + 1}`.padStart(2, '0');
-  const day =`${date.getDate()}`.padStart(2, '0');
-  return `${year}-${month}-${day}`
-}
+//function getDate() {
+//  const date = new Date();
+//  const year = date.getFullYear();
+//  const month = `${date.getMonth() + 1}`.padStart(2, '0');
+//  const day =`${date.getDate()}`.padStart(2, '0');
+//  return `${year}-${month}-${day}`
+//}
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -18,6 +18,7 @@ function getDate() {
   });
   const page = await browser.newPage();
   await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
-  await page.pdf({path: '/pdf/hackernews-' + getDate() + '.pdf', format: 'A4'});
+//  await page.pdf({path: '/pdf/hackernews-' + getDate() + '.pdf', format: 'A4'});
+  await page.pdf({path: '/pdf/hackernews.pdf', format: 'A4'});
   await browser.close();
 })();
